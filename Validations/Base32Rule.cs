3215d003d64507695fdb0cf8b5a6a1e6a9f741f5
Khang V Phan <kphan000@cs.washington.edu>
2023-02-05 19:35:24 -0800

    Initial complete application code

---
+++ Validations/Base32Rule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using OtpNet;
+
+namespace XIVQuickLauncherOTP.Validations;
+
+public class Base32Rule : ValidationRule
+{
+    public int MinBytes { get; set; }
+    public int MaxBytes { get; set; }
+
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        var bytes = new Span<byte>(new byte[MaxBytes + 1]);
+        try
+        {
+            var bytesWritten = Base32Encoding.ToBytes((string)value).Length;
+            if (bytesWritten < MinBytes || bytesWritten > MaxBytes)
+                return new ValidationResult(false, "Invalid base32 secret.");
+
+            return ValidationResult.ValidResult;
+        }
+        catch (Exception)
+        {
+            return new ValidationResult(false, "Invalid base32 secret.");
+        }
+    }
+}
---
+++ Validations/IntRangeRule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+
+namespace XIVQuickLauncherOTP.Validations;
+
+public class IntRangeRule : ValidationRule
+{
+    public int Min { get; set; }
+    public int Max { get; set; }
+
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        var val = 0;
+
+        try
+        {
+            if (((string)value).Length > 0)
+                val = int.Parse((string)value);
+        }
+        catch (Exception)
+        {
+            return new ValidationResult(false,
+                $"Please enter a number in the range: {Min}-{Max}.");
+        }
+
+        if (val < Min || val > Max)
+            return new ValidationResult(false,
+                $"Please enter a number in the range: {Min}-{Max}.");
+
+        return ValidationResult.ValidResult;
+    }
+}
